383bd13d7a3630f7d55a00a1edf091090b1c43a8
chronosxvi <arvin.peralta@gmail.com>
2018-03-15 11:22:44 +0800

    exercise5

--- arvin_exercise2.rb
+++ arvin_exercise2.rb
@@ -2,7 +2,7 @@
 #==========
 #using the for loop, write a ruby program that will print all numbers from 100 to 1
 
-=begin
+=begin version 1
 def countdown(x)
 	for i in x.downto 1 do
 		puts x
@@ -13,14 +13,13 @@ end
 countdown(100)
 =end
 
+i = 100
+
 def exercise_2(i)
 	for i in i.downto 1 do
 		puts i
 	end
 end
 
-puts "Count to zero from what number?"
-var = gets.chomp.to_i
-
-exercise_2(var)
+exercise_2(i)
 
--- arvin_exercise_1.rb
+++ arvin_exercise_1.rb
@@ -2,13 +2,10 @@
 #==========
 #using the for loop, write a ruby program that will print all numbers from 1 to 100
 
-def exercise_1(i)
-	for i in 1.upto i do
+def exercise_1()
+	for i in 1.upto 100 do
 		puts i
 	end
 end
 
-puts "Count to what number?"
-var = gets.chomp.to_i
-
-exercise_1(var)
+exercise_1()
