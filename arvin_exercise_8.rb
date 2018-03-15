42efa4566d440241f53d724b590c9c8d0b02e119
chronosxvi <arvin.peralta@gmail.com>
2018-03-15 11:31:57 +0800

    exercise 8

---
+++ arvin_exercise_8.rb
@@ -0,0 +1,12 @@
+#exercise 8
+#==========
+#using a while loop, write a ruby program that will print all the odd numbers from 200 to 100.
+
+x = 200
+
+while x >= 100 do
+	if x.odd? then
+		puts x
+	end
+	x -= 1
+end
