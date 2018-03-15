54ef1efda5b39f0bf736940e168dc6c74d53073f
Finn <finn.rothacker@gmail.com>
2018-03-15 11:47:40 +0800

    no change

--- _exercise_12.rb
+++ _exercise_12.rb
@@ -1,5 +1,4 @@
 #using a until loop, write a ruby program that will print all the odd numbers from 200 to 100.
  l = 200
- until l < 100 do if l.odd?
-    puts l end
+ until l < 100 do if l.odd? and puts l end
       l -= 1 end
